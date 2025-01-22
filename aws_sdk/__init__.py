6314e03725602dcf0f1d730172708d077a029643
chesn-admin <chesney.nyame@outlook.com>
2025-01-22 20:52:51 +0000

    Uncomment SQS client and parameter output functions in the AWS SDK initialization

--- aws_sdk/__init__.py
+++ aws_sdk/__init__.py
@@ -1,6 +1,6 @@
 from .get_ssm_client import aws_get_ssm_client
 from .get_s3_client import aws_get_s3_client
 from .get_s3_resource import aws_get_s3_resource
-# from .get_sqs_client import aws_get_sqs_client
-# from .output_ssm_parameters import output_aws_parameters, fetch_aws_parameters
+from .get_sqs_client import aws_get_sqs_client
+from .output_ssm_parameters import output_aws_parameters, fetch_aws_parameters
 
